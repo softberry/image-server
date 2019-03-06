6d84287291887de2ad9264218578d8b5e28b340b
softberry <emre.sakarya@softberry.de>
2019-03-06 17:22:03 +0100

    list of available images as json array

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const  express  = require("express");
 const app = express();
 const  router  = require("./routes");
 
-app.use("/app", router);
+app.use("/memory", router);
 
 app.listen(80, () => {
   console.log("Ready");
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { ImagesList } = require("../lib");
 
 router.get("/list/:count", (req, res, next) => {
-  res.json({ id: req.params.count, test:process.env });
-  next();
+  ImagesList()
+    .then(files => {
+      res.json(files);
+    })
+    .catch(next);
 });
 
 module.exports = router;
